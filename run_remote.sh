3b39cbc60e9e31289a1c813541ff359f873a57dd
Markus Klemm <markus@mklemm.org>
2025-11-27 19:51:10 +0100

    restart bluetooth on run_remote

--- run_remote.sh
+++ run_remote.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-IP=192.168.0.103
+IP=192.168.0.101
 ssh $IP -t "mkdir -p ~/ble"
 scp *.py $IP:~/ble
 scp bt_auto_advertice.sh $IP:~/ble
-scp bt_auto_advertice.sh $IP:~/ble
 # sudo systemctl disable G08_ble.service
 ssh $IP -t "sudo systemctl stop G08_ble.service" || true
+ssh $IP -t "sudo systemctl restart bluetooth"
 ssh $IP -t "chmod +x ~/ble/bt_auto_advertice.sh"
 ssh $IP -t "sudo ~/ble/bt_auto_advertice.sh"
 ssh $IP -t "cd ~/ble && sudo python3 main.py"
